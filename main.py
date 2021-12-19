11753880066dea5ca4ec84e31d2ec5e84cc8466d
Dmitry <babanov91@gmail.com>
2021-12-19 20:47:38 +0500

    исклчили глобавльные переменные. декомпозировали код

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
 list_file = ['1.txt', '2.txt', '3.txt']  # список файлов в каталоге
-dict_file_data = {}  # словарь {файл: содержание}
-dict_file_count = {}  # словарь {файл: количество строк}
 
 
 def get_count(file_name):
@@ -21,24 +19,36 @@ def get_data(file_name):
     return list_file_data
 
 
-for i in list_file:
-    dict_file_data[i] = get_data(i)
+def create_dict_content():
+    file_data = {}  # словарь {файл: содержание}
+    for i in list_file:
+        file_data[i] = get_data(i)
+    return file_data
 
 
-for i in list_file:
-    dict_file_count[i] = get_count(i)
+def sorted_dict_count():
+    dict_file_count = {}  # словарь {файл: количество строк}
+    sorted_dict_count = {}
+
+    for i in list_file:
+        dict_file_count[i] = get_count(i)
+
+        sorted_keys = sorted(dict_file_count, key=dict_file_count.get)
+
+    for w in sorted_keys:
+        sorted_dict_count[w] = dict_file_count[w]
+
+    return sorted_dict_count
 
 
-sorted_dict_count = {}
-sorted_keys = sorted(dict_file_count, key=dict_file_count.get)
+def write_file():
+    with open('result.txt', 'a') as document:
+        for key, values in sorted_dict_count().items():
+            document.write(f'{key}\n{values}\n')
+            for value in create_dict_content()[key]:
+                document.write(f'{value}\n')
 
-for w in sorted_keys:
-    sorted_dict_count[w] = dict_file_count[w]
+    print('Приложение успешно отработало, итоговое решение смотрите в файле "result.txt"')
 
-with open('result.txt', 'a') as document:
-    for key, values in sorted_dict_count.items():
-        document.write(f'{key}\n{values}\n')
-        for value in dict_file_data[key]:
-            document.write(f'{value}\n')
 
-print('Приложение успешно отработало, итоговое решение смотрите в файле "result.txt"')
+write_file()
